1e4a352bc3ee3129c99fe5c3ce6ab4a60f9707d1
Dinesh <dineshveguru123@gmail.com>
2022-02-26 00:00:25 +0530

    final commit

--- index.js
+++ index.js
@@ -1,4 +1,23 @@
 document.addEventListener("DOMContentLoaded", () => {
-  document.querySelector("h1").innerHTML = "light mode";
-  document.querySelector(".container").style.backgroundColor = "#e5e7e9";
+  const toggle = document.querySelector("#test");
+  const heading = document.querySelector("h1");
+  heading.innerHTML = "light mode";
+  const block = document.querySelector(".container");
+  block.style.backgroundColor = "#f0f8ff";
+  const bg = document.querySelector("body");
+  toggle.onclick = () => {
+    if (heading.innerHTML === "light mode") {
+      heading.innerHTML = "dark mode";
+      heading.style.color = "#fff";
+      toggle.src = "lamp.png";
+      bg.style.backgroundColor = "#141414";
+      block.style.backgroundColor = "#363636";
+    } else {
+      heading.innerHTML = "light mode";
+      block.style.backgroundColor = "#f0f8ff";
+      bg.style.backgroundColor = "#fff";
+      heading.style.color = "#000";
+      toggle.src = "moon.png";
+    }
+  };
 });
