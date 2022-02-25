6aeb5e44f88d129ef9165a3e30e1ae76450ae7d6
Dinesh <dineshveguru123@gmail.com>
2022-02-25 19:42:26 +0530

    intial commit

---
+++ index.js
@@ -0,0 +1,4 @@
+document.addEventListener("DOMContentLoaded", () => {
+  document.querySelector("h1").innerHTML = "light mode";
+  document.querySelector(".container").style.backgroundColor = "#e5e7e9";
+});
